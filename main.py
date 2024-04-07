958f73ac334daffd48459971399cedde4a831607
justmaruforu145 <166305908+justmaruforu145@users.noreply.github.com>
2024-04-07 17:43:37 +0300

    moments

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import numpy as np
 import cv2 as cv
-from picture_editing import VideoEditor
+from util.picture_edit import VideoEditor
 
 cap = cv.VideoCapture("Traffic IP Camera video (1).mp4")
 
@@ -17,11 +17,20 @@ while cap.isOpened():
     frame = VideoEditor.frame_edit(frame, 1)
 
     mask = bg_subs.apply(frame, learningRate=learning_rate)
-    mask = VideoEditor.mask_edit(mask, 5, 1)
+
     out = frame.copy()
     #result = cv.bitwise_and(out, out, mask=mask)
     #cv.imshow('res', mask)
-    VideoEditor().find_ctr(mask, out)
+
+    ret, thresh1 = cv.threshold(mask, 50, 255, cv.THRESH_BINARY)
+    filter_image = VideoEditor.filter_image(thresh1, 5,)
+    editer = VideoEditor.mask_edit(filter_image, 3, 3)
+   # cv.imshow('1', thresh1)
+   # cv.imshow('2', filter_image)
+   # cv.imshow('3', editer)
+
+
+    VideoEditor().find_ctr(editer, out)
     cv.imshow('input', out)
     c = cv.waitKey(10)
     if c == 27:
