871a2d61b9ec241756cb40e9c4414d3930ed6176
justmaruforu145 <166305908+justmaruforu145@users.noreply.github.com>
2024-04-14 17:43:09 +0300

    add interface

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ import numpy as np
 import cv2 as cv
 from util.picture_edit import VideoEditor
 
-cap = cv.VideoCapture("Traffic IP Camera video (1).mp4")
+cap = cv.VideoCapture("videoframe.mp4")
 
 bg_subs = cv.createBackgroundSubtractorMOG2()
 history = 300
 learning_rate = 1.0 / history
+
+arr_count_old = 0
+s = 0
 
 while cap.isOpened():
     ret, frame = cap.read()
@@ -19,19 +22,28 @@ while cap.isOpened():
     mask = bg_subs.apply(frame, learningRate=learning_rate)
 
     out = frame.copy()
+    out_frame = frame.copy()
     #result = cv.bitwise_and(out, out, mask=mask)
     #cv.imshow('res', mask)
 
     ret, thresh1 = cv.threshold(mask, 50, 255, cv.THRESH_BINARY)
-    filter_image = VideoEditor.filter_image(thresh1, 5,)
+    filter_image = VideoEditor.filter_image(thresh1, 3)
     editer = VideoEditor.mask_edit(filter_image, 3, 3)
    # cv.imshow('1', thresh1)
    # cv.imshow('2', filter_image)
    # cv.imshow('3', editer)
 
+    arr_count = VideoEditor().find_ctr_center(editer, out)
+    count = VideoEditor().car_check(arr_count)
+    if arr_count_old == 1 and count == 0:
+        s += 1
+    arr_count_old = count
+    VideoEditor().interface(out, s)
+    #cv.imshow('input', filter_image)
+    cv.imshow('input1', out)
 
-    VideoEditor().find_ctr(editer, out)
-    cv.imshow('input', out)
+
+
     c = cv.waitKey(10)
     if c == 27:
         break
--- util/picture_edit.py
+++ util/picture_edit.py
@@ -5,11 +5,11 @@ import numpy as np
 class VideoEditor:
     @staticmethod
     def frame_edit(frame, scale):
-        frame = cv.resize(frame, None, fx = scale, fy = scale, interpolation = cv.INTER_AREA)
+        frame = cv.resize(frame, None, fx=scale, fy=scale, interpolation=cv.INTER_AREA)
         return frame
 
     @staticmethod
-    def filter_image(frame, kernel, iterations):
+    def filter_image(frame, kernel):
         kernel = np.ones((kernel, kernel), np.uint8)
         opening = cv.morphologyEx(frame, cv.MORPH_OPEN, kernel)
         return opening
@@ -22,16 +22,43 @@ class VideoEditor:
         return dilation
 
     @staticmethod
-    def find_ctr(frame, res):
+    def find_ctr_center(frame, res):
         ret, thresh = cv.threshold(frame, 170, 255, 0)
         contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+
+        out = []
 
         for c in contours:
             M = cv.moments(c)
             if M['m00']>1000:
                 cX = int(M['m10']/M['m00'])
                 cY = int(M['m01'] / M['m00'])
+                out.append((cX, cY))
                 cv.circle(res, (cX, cY), 10, (0, 0, 255), -1)
+        cv.drawContours(res, contours, -1, (0, 255, 0), 3)
+        return out
+
+    @staticmethod
+    def car_check(arr_of_centers):
+        XMIN = 340
+        XMAX = 490
+        YMIN = 400
+        YMAX = 480
+        k = 0
+        for center in arr_of_centers:
+            x, y = center
+            if (XMIN <= x <= XMAX) and (YMIN <= y <= YMAX):
+                k += 1
+        return k
+
+    @staticmethod
+    def interface(frame, label):
+
+        string_out = f'cars q: {label}'
+        cv.rectangle(frame, (340, 480), (490, 400), (255, 0, 0), 5)
+        cv.putText(frame, string_out, (70, 80), cv.FONT_HERSHEY_SIMPLEX, 1, (20, 20, 255), 2, cv.LINE_AA)
+
+
 
         #moments = cv2.moments(frame)
 
@@ -39,8 +66,7 @@ class VideoEditor:
         #dM10 = moments['m10']
         #darea = moments['m00']
 
-        if darea > 100:
-            x = int(dM10 / darea)
-            y = int(dM10 / darea)
-            cv.circle(res, (x, y), 10, (0, 0, 255), -1)
-        cv.drawContours(res, contours, -1, (0, 255, 0), 3)
+        #if darea > 100:
+        #    x = int(dM10 / darea)
+        #    y = int(dM10 / darea)
+        #    cv.circle(res, (x, y), 10, (0, 0, 255), -1)
